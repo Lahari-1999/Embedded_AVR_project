381bd36e476557b7b6b33457781b4f65330b8a9b
Lahari-1999 <laharilucky99@gmail.com>
2021-04-24 11:06:07 +0530

    added main.c

--- main.c
+++ main.c
@@ -1,10 +1,37 @@
+
+/**
+ * @file main.c
+ * @author vootla lahari sfid:260904 
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+
 #include <avr/io.h>
 #include <util/delay.h>
 
+
+
+/**
+ * @brief  A main function to operate on button sensor and heater sensor to turn on LED
+ * 
+ * @return int 
+ */
+
 int main(void)
+
 {
 
-    // Insert code
+
+/**
+ * @brief Main code
+ * 
+ */
     DDRB |=(1<<PB0);
 
 
@@ -21,15 +48,14 @@ int main(void)
 
     if(!((PIND&(1<<PD0)) || (PIND&(1<<PD1)))){
 
-     PORTB |=(1<<PB0);
+     PORTB |=(1<<PB0); //make 0th bit of port B as 1, makes led glow
         _delay_ms(2000);
     }
     else{
-        PORTB&=~(1<<PB0);
-         _delay_ms(2000);
+        PORTB&=~(1<<PB0); // make led off
+         _delay_ms(2000); 
     }
-
-
 }
+
     return 0;
 }
