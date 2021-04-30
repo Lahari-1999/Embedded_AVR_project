e555adf1e2720e0228a4352de160bcf2af6626d3
Lahari-1999 <laharilucky99@gmail.com>
2021-04-30 10:32:08 +0530

    Documentation

--- Documentation/html/search/all_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['uartinit_9',['UARTinit',['../activity4_8c.html#a9f9cf5f74f13d365a3d9ee31d973746d',1,'activity4.c']]],
-  ['uartreadchar_10',['UARTreadchar',['../activity4_8c.html#ae83a087608eca309eff2233e1ac50a08',1,'activity4.c']]],
-  ['uartwritecharacter_11',['UARTwritecharacter',['../activity4_8c.html#afca1825b81cb225b2473f8b1b5ae7ebb',1,'activity4.c']]]
-];
--- Documentation/html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pwmoutput_19',['PWMoutput',['../activity3_8c.html#a0fd424c5797b6dd233c2a4a402a66e7d',1,'activity3.c']]]
-];
